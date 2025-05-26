f3ce120bebaa065077b7338f18dfacf4bf965e2f
Marcel Reig <marcelreig@gmail.com>
2025-05-26 19:16:52 +0000

    edit

--- src/app.js
+++ src/app.js
@@ -1,33 +1,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// window.onload = function () {
-//   //write your code here
-//   const suits = ["hearts", "diamonds", "clubs", "spades"];
-//   const numbers = [
-//     "A",
-//     "2",
-//     "3",
-//     "4",
-//     "5",
-//     "6",
-//     "7",
-//     "8",
-//     "9",
-//     "10",
-//     "J",
-//     "Q",
-//     "K",
-//   ];
-//   let randomSuit = Math.floor(Math.random() * suits.length);
-//   let randomNumber = Math.floor(Math.random() * numbers.length);
-//   console.log(randomSuit);
-//   console.log(randomNumber);
-
-//   let card = document.querySelector(".card");
-//   let cardNumber = document.querySelector(".card-number");
-//   let cardSuit = document.querySelector(".card-suit");
-// };
+window.onload = function () {
+  //write your code here
+  const suits = ["heart", "diamond", "club", "spade"];
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  let randomSuit = Math.floor(Math.random() * suits.length);
+  let randomNumber = Math.floor(Math.random() * numbers.length);
+  let cardNumber = document.getElementById("number");
+  let cardSuit = document.getElementById("card");
+  cardNumber.innerHTML = numbers[randomNumber];
+  cardSuit.classList.add(suits[randomSuit]);
+};
